32404a604346308dcabad665ff49d290e0debfd7
Srikanth Bangalore <srib@yahoo-inc.com>
2016-07-06 10:39:45 -0700

    updated some files

--- modules/chessthink/src/Controller/ChessThinkController.php
+++ modules/chessthink/src/Controller/ChessThinkController.php
@@ -19,6 +19,33 @@ use Drupal\Component\Utility\SafeMarkup;
  * Controller routines for Chess think pages.
  */
 class ChessThinkController extends ControllerBase{
+  public function purdymethod() {
+    $response = new Response();
+    $response->headers->set('Expires', 'Sun, 19 Nov 1978 05:00:00 GMT');
+    $response->headers->set('Cache-Control', 'must-revalidate');
+    $response->headers->set('Content-Type', 'text/html; charset=utf-8');
+
+    $fen = '3r1rk1/1bq2ppp/p1p1pb2/1pPnN1Nn/3P3P/PP1B2P1/2QB1PK1/4R2R b - - 0 1';
+    $opponentsmove = 'h7h6';
+    $answer = 'd3h7';
+    $general_bg = '1... h6 2. Bh7+ Kh8 3. Bg8 g6 4. Bxf7 hxg5 5. Qxg6';
+    $posid = 1;
+
+    // Read actual feed from file.
+    $file_name = drupal_get_path('module', 'chessthink') . '/purdymethod.html';
+    $handle = fopen($file_name, 'r');
+    $feed = fread($handle, filesize($file_name));
+    fclose($handle);
+
+    $feed = str_replace('SECRETFEN', $fen, $feed);
+    $feed = str_replace('SECRETOPPMOVE', $opponentsmove, $feed);
+    $feed = str_replace('SECRETMYMOVE', $answer, $feed);
+    $feed = str_replace('SECRETGENBG', $general_bg, $feed);
+    $feed = str_replace('SECRETPOSID', $posid, $feed);
+
+    $response->setContent($feed);
+    return $response;
+  }
   public function think() {
     $response = new Response();
     $response->headers->set('Expires', 'Sun, 19 Nov 1978 05:00:00 GMT');
