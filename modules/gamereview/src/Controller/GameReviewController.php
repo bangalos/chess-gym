50fc4d92504503331ecb2a1a5b8e816ee09ad45d
Srikanth Bangalore <srib@yahoo-inc.com>
2016-07-09 19:21:00 -0700

    ibiza commit

--- modules/gamereview/src/Controller/GameReviewController.php
+++ modules/gamereview/src/Controller/GameReviewController.php
@@ -19,15 +19,55 @@ use Drupal\Component\Utility\SafeMarkup;
  * Controller routines for Game Review pages.
  */
 class GameReviewController extends ControllerBase{
-  public function gamereview() {
+  public function gamereview($gamedir="rudolf_spielmann", $gamefile="game_9") {
     $response = new Response();
     $response->headers->set('Expires', 'Sun, 19 Nov 1978 05:00:00 GMT');
     $response->headers->set('Cache-Control', 'must-revalidate');
     $response->headers->set('Content-Type', 'text/html; charset=utf-8');
 
+    //TODO: Assume that the pgn filename is magically available in the following variable:
+    $current_uri = \Drupal::request()->getRequestUri();
+    if (preg_match("/^(.+?)amereview\/(.+?)\/(.+?)_(.+?)$/", $current_uri, $match)) {
+      $gamedir = $match[2];
+      $gamefile = "$match[3]_$match[4]";
+      $nextgamefile = "$match[3]_" . ($match[4] + 1);
+      $defaultgamefile = "$match[3]_1";
+      $sitebaseuri = "$match[1]amereview/$gamedir";
+    }
+    $pgn_file_name = "rudolf_spielmann/game_9";
+    if ($gamedir != "" && $gamefile != "") {
+      $pgn_file_name = "$gamedir/$gamefile";
+    }
+    $pgn_file_name = DRUPAL_ROOT . "/sites/default/files/chessgym/gamereview/" . $pgn_file_name;
+    $nextpgn_file_name = DRUPAL_ROOT . "/sites/default/files/chessgym/gamereview/$gamedir/$nextgamefile";
+    $tmphandle = fopen($nextpgn_file_name, 'r');
+    $nextgameurl = "$sitebaseuri/$defaultgamefile";
+    if ($tmphandle) {
+      $nextgameurl = "$sitebaseuri/$nextgamefile";
+    }
+    // Read actual feed from file.
+    $handle = fopen($pgn_file_name, 'r');
+    $feed = fread($handle, filesize($pgn_file_name));
+    fclose($handle);
+    $pgnlines = explode("\n", $feed);
+    $pgn = "";
+    $general_bg = "";
+    $threshold = 11;
+    foreach ($pgnlines as $line) {
+      $line = rtrim($line);
+      if (!preg_match("/\[/", $line) && $line != "") {
+        if ($pgn == "") {$pgn = $line;} else { $pgn = "$pgn $line";}
+      } else {
+        $general_bg = "$general_bg<br/>$line";
+      }
+      if (preg_match("/Black \"Rudolf Spielmann\"/", $line)) {
+        $threshold = 12;
+      }
+    }
+    $feed = ""; //Reset it.
+
+
     $fen = 'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - c3 0 19';
-    $general_bg = 'Rudolf Spielmann vs Baldur Hoenlinger 1929. Caro Kann Main Line. <a href="http://www.chessgames.com/perl/chessgame?gid=1130925">Chess Game</a>';
-    $pgn = '1.e4 e5 2.Nf3 Nc6 3.Bc4 Bc5 4.b4 Bxb4 5.c3 Ba5 6.d4 exd4 7.O-O d3 8.Qb3 Qf6 9.e5 Qg6 10.Re1 Nge7 11.Ba3 b5 12.Qxb5 Rb8 13.Qa4 Bb6 14.Nbd2 Bb7 15.Ne4 Qf5 16.Bxd3 Qh5 17.Nf6+ gxf6 18.exf6 Rg8 19.Rad1 Qxf3 20.Rxe7+ Nxe7 21.Qxd7+ Kxd7 22.Bf5+ Ke8 23.Bd7+ Kf8 24.Bxe7# 1-0';
 
     // Read actual feed from file.
     $file_name = drupal_get_path('module', 'gamereview') . '/gamereview_core.html';
@@ -38,6 +78,8 @@ class GameReviewController extends ControllerBase{
     $feed = str_replace('SECRETFEN', $fen, $feed);
     $feed = str_replace('SECRETGENBG', $general_bg, $feed);
     $feed = str_replace('SECRETPGN', $pgn, $feed);
+    $feed = str_replace('SECRETTHRESHOLD', $threshold, $feed);
+    $feed = str_replace('SECRETNEXT', $nextgameurl, $feed);
 
     $response->setContent($feed);
     return $response;
